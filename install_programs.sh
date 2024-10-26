a17767d1b763d2936816556df464fcb2f58afc52
dragon09993 <danielborges59@gmail.com>
2024-10-25 22:46:40 -0500

    adding github setup instructions

--- install_programs.sh
+++ install_programs.sh
@@ -1,5 +1,5 @@
     #!/bin/bash                                                                                                                                                                                                    
                                                                                                                                                                                                                    
     # Install required programs on Arch Linux                                                                                                                                                                      
-    sudo pacman -Syu --noconfirm kitty i3 eh tty-clock                                                                                                                                                             
+    sudo pacman -Syu --noconfirm kitty i3 eh tty-clock git vim                                                                                                                                                            
                                                           
