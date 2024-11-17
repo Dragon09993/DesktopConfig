4050efe1398b8e75d2aae0516b3822f224dcc5b2
Dragon09993 <danielborges59@gmail.com>
2024-11-17 11:03:22 -0600

    Update install_programs.sh

--- install_programs.sh
+++ install_programs.sh
@@ -1,5 +1,5 @@
     #!/bin/bash                                                                                                                                                                                                    
                                                                                                                                                                                                                    
     # Install required programs on Arch Linux                                                                                                                                                                      
-    sudo pacman -Syu --noconfirm kitty i3 eh tty-clock git vim                                                                                                                                                            
+    sudo pacman -Syu --noconfirm kitty i3 feh tty-clock git vim                                                                                                                                                            
                                                           
