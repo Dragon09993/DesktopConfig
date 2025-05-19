1032bb495f9c557c07df3bf52e1072b9c2ea8205
Dragon09993 <danielborges59@gmail.com>
2025-05-18 22:21:39 -0500

    Update settings-launcher.sh
    adding gnome system monitor

--- .config/i3/settings-launcher.sh
+++ .config/i3/settings-launcher.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-choice=$(printf "Display\nNetwork\nAudio\nKeyboard" | dmenu -i -p "Settings")
+choice=$(printf "Monitor\nDisplay\nNetwork\nAudio\nKeyboard" | dmenu -i -p "Settings")
 
 case "$choice" in
+  Monitor) gnome-system-monitor ;;
   Display) arandr ;;
   Network) nm-connection-editor ;;
   Audio) pavucontrol ;;
   Keyboard) lxinput ;;
 esac
+
+
+
