216d90e2746b9d696e6da98bf75345df48b66d2f
Dragon09993 <danielborges59@gmail.com>
2025-06-25 15:36:10 -0500

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,75 +1,77 @@
--- =====[ BASIC OPTIONS ]=====
+-- =====[ CORE OPTIONS ]=====
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+
 vim.opt.wrap = false
-vim.opt.showmode = false
 vim.opt.scrolloff = 4
 vim.opt.sidescrolloff = 8
-vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.laststatus = 3
+vim.opt.showmode = false
 
--- Tabs & indentation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.incsearch = true
 vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
--- Clipboard
-vim.opt.clipboard = "unnamedplus"
-
--- Backups / swap / undo
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-
--- Mouse
-vim.opt.mouse = "a"
-
--- UI
-vim.opt.laststatus = 2
-vim.opt.signcolumn = "yes"
-
--- Leader key
 vim.g.mapleader = " "
 
--- =====[ KEYMAPS ]=====
+-- =====[ KEYBINDINGS ]=====
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+-- File ops
 map("n", "<leader>w", ":w<CR>", opts)
 map("n", "<leader>q", ":q<CR>", opts)
 map("n", "<leader>x", ":x<CR>", opts)
+
+-- Buffer navigation
 map("n", "<leader>bn", ":bnext<CR>", opts)
 map("n", "<leader>bp", ":bprevious<CR>", opts)
 map("n", "<leader>bd", ":bdelete<CR>", opts)
 
--- Window navigation
+-- Window nav (vim-tmux-style)
 map("n", "<C-h>", "<C-w>h", opts)
 map("n", "<C-j>", "<C-w>j", opts)
 map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
--- =====[ COLOR THEME: FreeBSD-Inspired ]=====
+-- =====[ COLORS — FreeBSD-RED AESTHETIC ]=====
 vim.cmd [[
-    highlight Normal       guibg=NONE guifg=#ffffff
-    highlight LineNr       guifg=#ff5f5f
-    highlight CursorLineNr guifg=#ff0000 gui=bold
-    highlight CursorLine   guibg=#1c1c1c
-    highlight Comment      guifg=#ff8787
-    highlight Statement    guifg=#ff0000 gui=bold
-    highlight Search       guibg=#ff0000 guifg=#000000
-    highlight Visual       gui=reverse
-    colorscheme default
+  highlight Normal         guibg=#1E1328 guifg=#F7E3D8
+  highlight LineNr         guifg=#7A667A guibg=#1E1328
+  highlight CursorLineNr   guifg=#FF6F61 gui=bold
+  highlight CursorLine     guibg=#2A1C3A
+  highlight VertSplit      guifg=#7A667A guibg=#1E1328
+  highlight StatusLine     guifg=#F7E3D8 guibg=#FF6F61 gui=bold
+  highlight StatusLineNC   guifg=#7A667A guibg=#1E1328
+  highlight Visual         guibg=#FF6F61 guifg=#1E1328 gui=none
+  highlight Search         guibg=#FF9B6B guifg=#1E1328
+  highlight Pmenu          guibg=#2A1C3A guifg=#F7E3D8
+  highlight PmenuSel       guibg=#FF6F61 guifg=#1E1328
+  highlight Comment        guifg=#AA7788 italic
+  highlight Keyword        guifg=#FF6F61 gui=bold
+  highlight Identifier     guifg=#FF9B6B
+  highlight Function       guifg=#F7E3D8 gui=bold
+  highlight Constant       guifg=#FFBDBA
+  highlight String         guifg=#F79682
+  highlight Type           guifg=#FF6F61 gui=italic
+  highlight Todo           guifg=#1E1328 guibg=#FF6F61 gui=bold
+  colorscheme default
 ]]
 
--- =====[ FILETYPE + PLUGIN SUPPORT ]=====
+-- =====[ FILETYPE / SYNTAX ]=====
 vim.cmd [[
-    filetype plugin indent on
-    syntax enable
+  filetype plugin indent on
+  syntax enable
 ]]
